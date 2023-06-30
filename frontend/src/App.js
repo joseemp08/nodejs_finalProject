93009b9aaa682e2899a97d18a9d1b1c743455746
jose <jose.moreno2099@gmail.com>
2023-06-29 21:53:59 -0600

    map box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+//import * as React from 'react';
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 function App() {
-  return (
-    <div className="App">
-      hello
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+  return(
+    <div className='App'>
+      <ReactMapGL
+      {...viewport}
+      oneViewportChange={nextViewport => setViewport(nextViewport)}
+      />
     </div>
   );
 }
